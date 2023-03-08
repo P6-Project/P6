8de2abad52b230d85edbb6593b17d8ce703e64a5
Magnus J. Harder <26723180+MJHC@users.noreply.github.com>
2023-03-08 10:50:51 +0100

    Dataframe format and time split (#2)
    * machine database
    
    * dataframes and updated model
    
    * removed mendeley
    
    * rename target

--- src/model.py
+++ src/model.py
@@ -1,33 +1,29 @@
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import random
+import pandas as pd
+
 
 #loading data
 print("loading cars")
-with open("../cars.pkl", "rb") as f:
-    cars: dict = pickle.load(f)
+with open("./loxam.pkl", "rb") as f:
+    dataframe: pd.DataFrame = pickle.load(f)
 	
-data = []
-target = []
 
-#Formatting Data
-print("formatting cars")
-for i, (car, car_data) in enumerate(cars.items(), 1):
-    for d in car_data:
-        random.shuffle(d)
-        data.append(d)
-        target.append(i)
-minlen = min(len(d) for c, cd in cars.items() for d in cd) - 1
-data = [d[:minlen] for d in data]
+dataframe["ID"] = dataframe["ID"].apply(lambda x: int(x, 16))
+target = dataframe["Name"]
+dataframe = dataframe.drop("Name", axis=1).drop("Source", axis=1).drop("Action", axis=1)
+
+
 
 # Splitting data
 print("splitting data")
-data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.2)
+data_train, data_test, target_train, target_test = train_test_split(dataframe, target, test_size=0.2)
 
 #  Setup and training
 print("setup and training")
 clf = RandomForestClassifier()
+
 clf.fit(data_train, target_train)
 print("Prediction: ", clf.predict(data_test))
 print("Target Values: ", target_test)
