e13d0f0718fc1f58ca6d3de34867b37a524d45d2
Lasse <lass5588@icloud.com>
2023-03-09 19:37:21 +0100

    Updated digram properties.

--- src/histogram.py
+++ src/histogram.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def create_dict_can_pkl():
-    df = pd.read_pickle("../../data/loxam1.pkl")
+    df = pd.read_pickle("../../data/loxam.pkl")
 
     can_id_dict = dict()
     
@@ -18,7 +18,11 @@ def create_dict_can_pkl():
 def create_bar_diagram_can_ids(can_id_dict):
     plt.bar(can_id_dict.keys(), can_id_dict.values(), 0.5)
     plt.xticks(rotation='vertical')
-    plt.title("Can ID data")
+    plt.title("All Loxam CAN Readings") # Insert vehicle name.
+    plt.xlabel('CAN Id')
+    plt.ylabel('Quantity')
+    plt.margins(0.01)
+    plt.tight_layout() # Else it cuts off half of the bottom text.
     return plt
 
 diagram = create_bar_diagram_can_ids(create_dict_can_pkl())
