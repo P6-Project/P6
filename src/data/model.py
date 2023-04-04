7922903da92188679e42525e4cec2de01e475f60
mksm20 <mksm20@student.aau.dk>
2023-04-04 11:40:06 +0200

    removed unneccesary code

--- src/data/model.py
+++ src/data/model.py
@@ -5,19 +5,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
 def train_model() -> SVC:
+    train_dfs = []
+    test_dfs = []
     for filename in os.listdir("./data/dfs"):
-        train_dfs = []
-        test_dfs = []
-        for filename in os.listdir("./data/dfs"):
-            if filename.find("binaryMatrix") != -1:
-                try:
-                    df = pd.read_pickle(os.path.join("./data/dfs", filename))
-                    train_df, test_df = train_test_split(df, test_size=0.3, random_state=42)
-                    train_dfs.append(train_df)
-                    test_dfs.append(test_df)
-                except ValueError:
-                    continue
-
+        if filename.find("binaryMatrix") != -1:
+            try:
+                df = pd.read_pickle(os.path.join("./data/dfs", filename))
+                train_df, test_df = train_test_split(df, test_size=0.3, random_state=42)
+                train_dfs.append(train_df)
+                test_dfs.append(test_df)
+            except ValueError:
+                continue
+    
     train_data = pd.concat(train_dfs, axis=0, ignore_index=False).fillna(0)
     test_data = pd.concat(test_dfs, axis=0, ignore_index=False).fillna(0)
     X_train = train_data.iloc[:,:-1]
