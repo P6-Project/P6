1a4f68643403bda304c2170fc7a22703b2b66485
mksm20 <mksm20@student.aau.dk>
2023-04-07 13:45:07 +0200

    added predict and save model

--- src/data/modelRandomForrest.py
+++ src/data/modelRandomForrest.py
@@ -3,7 +3,9 @@ from sklearn.model_selection import train_test_split
 import os
 import pandas as pd
 
-def random_forrest_model():
+def random_forrest_model_creator():
+    if not os.path.exists("./data/models"):
+        os.makedirs("./data/models")
     dfs = []
     for filename in os.listdir("./data/dfs"):
         print(filename)
@@ -21,11 +23,30 @@ def random_forrest_model():
     X_train, X_test , y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     rf = RandomForestClassifier(n_estimators=100, random_state=42)
     rf.fit(X_train, y_train)
-    
+    save_model(rf, "randomForrest")
     predictions = rf.predict(X_test)
     
     accuracy = (predictions == y_test).mean()
     print("Accuracy: " + str(accuracy))
     
+
+def save_model(model, filename):
+    pd.to_pickle(model, "./data/models/" + filename + ".pkl")
+
+
+def predict(model, data):
+    return model.predict(data)
+
 if __name__ == "__main__":
-    random_forrest_model()
+    #random_forrest_model_creator()
+    for files in os.listdir("./data/dfs"):
+        if files.find("timeNormalized") != -1:
+            resDict = {}
+            print(files)
+            df : pd.DataFrame = pd.read_pickle(os.path.join("./data/dfs", files))
+            res = predict(pd.read_pickle("./data/models/randomForrest.pkl"), df['ID'].apply(lambda x: int(x, 16)).to_frame())
+            for e in res:
+                resDict[e] = resDict.get(e, 0) + 1    
+            print(resDict)
+    
+    
