cceaea16cea4bc36097e929db4e058c7aafa9cd3
alexsteffensen <asteff20@student.aau.dk>
2023-04-19 12:26:27 +0200

    fixed test

--- test/test_files/processing_tests/test_data_checker.py
+++ test/test_files/processing_tests/test_data_checker.py
@@ -1,20 +1,20 @@
-from protocol_identifier.processing import get_data_range, check_data_point
+from protocol_identifier.verification import get_data_range, check_data_point
 import pandas as pd
 
 
 ranges = ["0 to 6425.5 ohm", "", "0 to 3", "0 to 250 %"]
 units = ["ohm", "V", "", "%"]
-values = ["3,342.2", "223", "5", "25"]
+values = ["3342.2", "223", "5", "25"]
 
 
 def test_get_data_range():
     df = pd.DataFrame(zip(ranges, units), columns=["Data Range", "Units"])
 
-    expected = [(0, 6425.5), None, (0, 3)]
+    expected = [{"min": 0, "max": 6425.5}, None, {"min": 0, "max": 3}, {"min": 0, "max": 250}]
     out = list()
     for index, row in df.iterrows():
         out.append(get_data_range(row["Data Range"], row["Units"]))
-
+    print(out)
     assert out == expected
 
 def test_data_point_checker():
