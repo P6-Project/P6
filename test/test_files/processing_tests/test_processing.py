3e96d177bb07607dbb4799605e7b9ecbc5b69904
MJHC <magnus@bonusfamilien.dk>
2023-05-03 11:04:03 +0200

    test fixed

--- test/test_files/processing_tests/test_processing.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from protocol_identifier.normalize import norm_time
-
-def test_norm_time():
-    input = ["13:37:00.100000", "13:37:00.300000", "13:37:00.400000", "13:37:00.700000", "13:37:00.900000", "13:37:01.200000"]
-    expected = [0, 200, 300, 0, 200, 500]
-    out = norm_time(input, 3)
-    assert out == expected
